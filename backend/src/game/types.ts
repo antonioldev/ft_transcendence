930d589c3d30d281e09d335c0e80d5d8ab744038
Tony <bauduchi@live.com>
2025-07-05 01:18:43 +0100

    ADD: new type of json messages

--- backend/src/game/types.ts
+++ backend/src/game/types.ts
@@ -10,3 +10,17 @@ export type GameState = {
 	paddle_right: {y: number, score: number};
 	ball: {x: number, y: number}
 }
+
+export type ClientMessage = {
+	type: 'join_game' | 'player_input';
+	gameMode?: 'singlePlayer' | 'two_player_remote';
+	side?: number;
+	direction?: 'left' | 'right' | 'stop';
+}
+
+export type ServerMessage = {
+	type: 'game_state' | 'side_assignment' | 'game_started' | 'error';
+	state?: GameState;
+	side?: number;
+	message?: string;
+}
