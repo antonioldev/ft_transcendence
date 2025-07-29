c1b46ec45c1b5fb4d72fa84a44bd72d9510bb29b
edenbeden98 <edenbeden98@gmail.com>
2025-07-29 07:02:55 +0100

    refactored Tournament class a little

---
+++ backend/src/network/Tournament.ts
@@ -0,0 +1,148 @@
+import { GameSession } from './GameSession.js'
+import { Game } from '../core/game.js';
+import { Client, Player } from '../models/Client.js';
+import { MessageType, GameMode } from '../shared/constants.js';
+import { PlayerInput, ServerMessage } from '../shared/types.js';
+
+class Match {
+	id: string = crypto.randomUUID();
+	round: number;
+	players: Player[] = [];
+	clients: Client[] = [];
+	game!: Game;
+
+	left!: Match;
+	right!: Match;
+	next: Match | null = null;
+
+	constructor (round: number) {
+		this.round = round;
+	}
+
+	add_player(player: Player) {
+		if (!this.players.includes(player)) {
+			this.players.push(player);
+		}
+		if (player.client !== null && !this.clients.includes(player.client)) {
+			this.clients.push(player.client);
+		}
+	}
+}
+
+export class Tournament extends GameSession {
+	match_map: Map<string, Match> = new Map();	// Maps id to match, used for easy insertion from client input
+	rounds: Map<number, Match[]> = new Map();	// Maps rounds to match[], used for easy traversal to run games
+	num_rounds: number;
+
+	constructor(mode: GameMode, game_id: string, capacity: number) {
+		super(mode, game_id);
+		this.player_capacity = capacity;
+		this.num_rounds = this._get_num_rounds(this.player_capacity);
+		this._create_rounds_map();
+		this._create_match_tree(new Match(this.num_rounds));
+	}
+
+	enqueue(input: PlayerInput, match_id?: string): void  {
+		if (!match_id) return ;
+		this.match_map.get(match_id)?.game.enqueue(input);
+	}
+
+	private _create_rounds_map() {
+		for (let i = 1; i <= this.num_rounds; i++) {
+			this.rounds.set(i, []);
+		}
+	}
+
+	// calculates the number of rounds from the number of players in the tournament
+	private _get_num_rounds(num_players: number) {
+		let round_count = 0;
+		while (num_players > 1) {
+			num_players /= 2;
+			round_count += 1;
+		}
+		return (round_count);
+	}
+
+	// creates the tournament tree structure and assigns each match to the rounds map
+	private _create_match_tree(current_match: Match) {
+		this.match_map.set(current_match.id, current_match);
+		this.rounds.get(current_match.round)?.push(current_match);
+
+		if (current_match.round === 1) return ;
+
+		current_match.left = new Match(current_match.round - 1);
+		current_match.right = new Match(current_match.round - 1)
+		
+		current_match.left.next = current_match;
+		current_match.right.next = current_match;
+
+		this._create_match_tree(current_match.left);
+		this._create_match_tree(current_match.right);
+	}
+
+
+	// assigns players to the matches in round_1
+	private _match_players(): void {
+		// TODO: randomize start order ??
+
+		const round_one = this.rounds.get(1);
+		if (!round_one) return ; // maybe throw err
+
+		for (let i = 0, j = 0; j != this.player_capacity; i++, j+=2) {
+			round_one[i].add_player(this.players[j]);
+			round_one[i].add_player(this.players[j+1]);
+		}
+	}
+
+	// runs all matches in a given round in parallel
+	private async _run_all(matches: Match[]) {
+		let winner_promises: Promise<Player>[] = [];
+
+		// run each match in parallel and await [] of match promises
+		for (const match of matches) {
+			this.assign_sides(match.players);
+			match.game = new Game(match.players, (message) => this.broadcast(message, match.clients));
+			let winner_promise: Promise<Player> = match.game.run();
+			winner_promises.push(winner_promise);
+		}
+		const winners = await Promise.all(winner_promises);
+
+		// promote winners once all promises are resolved
+		for (let i = 0; i < winners.length; i++) {
+			const match = matches[i];
+			const winner = winners[i];
+			match.next?.add_player(winner);
+		}
+	}
+
+	// runs each game in a round one by one and awaits each game before starting the next
+	private async _run_one_by_one(matches: Match[]) {
+		for (const match of matches) {
+			this.assign_sides(match.players);
+			match.game = new Game(match.players, this.broadcast.bind(this));
+			const winner = await match.game.run();
+			match.next?.add_player(winner);
+		}
+	}
+	
+	async start() {
+		if (this.running || this.players.length != this.player_capacity) return ;
+
+		this.running = true;
+		this._match_players();
+		
+		for (let round = 1; round <= this.num_rounds; round++) {
+			const matches = this.rounds.get(round);
+			if (!matches) return ; // maybe throw err
+	
+			if (this.mode == GameMode.TOURNAMENT_LOCAL) {
+				await this._run_one_by_one(matches);
+			}
+			else /* TOURNAMENT_REMOTE */ {
+				await this._run_all(matches);
+			}
+		}
+
+		// TODO: display final winner screen
+	}
+}
