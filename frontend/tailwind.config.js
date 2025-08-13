ccc74abd262ae36ba32f351104c0b1c63243eb79
Kelly Rocha Hayd <krocha-h@c1r7s1.42london.com>
2025-08-13 17:08:12 +0100

    WIP: new index file - Tailwind framework

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,21 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'tiny5-regular': ['"Tiny5"', 'sans-serif'],
+      },
+      colors: {
+        'gray-background': '#565656',
+        'light-green': '#B2FFA9',
+        'orange': '#FF4A1C',
+        'light-brown': '#81523F',
+        'dark-brown': '#3F2A2B'
+      }
+    }
   },
   plugins: [],
 }
