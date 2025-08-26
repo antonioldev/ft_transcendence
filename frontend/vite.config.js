bf6af7cb31250b6aee263d69a1c7ef10d2b064e9
Kelly Hayd <hayd.kelly@gmail.com>
2025-08-26 10:27:39 +0100

    feat: Add form-input and form-field-container classes for standardization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ export default defineConfig({
   root: './src',
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5173
+    }
   },
   build: {
     outDir: '../dist'
