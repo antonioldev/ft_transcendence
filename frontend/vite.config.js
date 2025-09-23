988683133afdefb28324f09ea6e2eeb5814b4cdb
Kelly Rocha Hayd <krocha-h@c1r2s1.42london.com>
2025-09-23 14:45:28 +0100

    git: update git ignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,3 +17,18 @@ export default defineConfig({
     postcss: './postcss.config.js'
   }
 })
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './src',
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  build: {
+    outDir: '../dist'
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
+})
