74b96164e79cb3e97e3ff25603e9a66c3b33e0f6
Kelly Rocha Hayd <krocha-h@c1r2s1.42london.com>
2025-09-23 14:35:36 +0100

    wip: rebase main

--- frontend/src/translations/translations.ts
+++ frontend/src/translations/translations.ts
@@ -150,13 +150,13 @@ export function updateLanguageDisplay(): void {
 
     // const tournamentInput2 = requireElementById<HTMLInputElement>(EL.PLAYER_SETUP.PLAYER2_NAME_TOURNAMENT);
 
-	// const tournamentLabel3 = document.querySelector('#tournament-setup label[for="player3-name-tournament"]');
-	// if (tournamentLabel3) tournamentLabel3.textContent = "Player 3 Name:";
+    const tournamentLabel3 = document.querySelector('#tournament-setup label[for="player3-name-tournament"]');
+    if (tournamentLabel3) tournamentLabel3.textContent = "Player 3 Name:"; // TODO: Add translation key for player3Name
 
     // const tournamentInput3 = requireElementById<HTMLInputElement>(EL.PLAYER_SETUP.PLAYER3_NAME_TOURNAMENT);
 
-	// const tournamentLabel4 = document.querySelector('#tournament-setup label[for="player4-name-tournament"]');
-	// if (tournamentLabel4) tournamentLabel4.textContent = "Player 4 Name:";
+    const tournamentLabel4 = document.querySelector('#tournament-setup label[for="player4-name-tournament"]');
+    if (tournamentLabel4) tournamentLabel4.textContent = "Player 4 Name:"; // TODO: Add translation key for player4Name
 
 	// const tournamentInput4 = requireElementById<HTMLInputElement>(EL.PLAYER_SETUP.PLAYER4_NAME_TOURNAMENT);
 	// tournamentInput4.placeholder = "Enter Player 4 name";
