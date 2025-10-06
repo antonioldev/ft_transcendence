16a89375966838f145da397d7917f79026e3a5c1
Kelly Rocha Hayd <krocha-h@c1r1s4.42london.com>
2025-10-06 11:08:34 +0100

    fix: regex error fixed in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -62,8 +62,7 @@ await fastify.register(fastifyCors, {
             /^https?:\/\/172\.(?:1[6-9]|2[0-9]|3[01])\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:8443)?$/,
             /^https?:\/\/192\.168\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:8443)?$/,
         
-            /^https:\/\/localhost$/,
-            /^http:\/\/localhost:\d+$/
+            /^https?:\/\/localhost(:\d+)?$/
         ];
         if (allowed.some(regexp => regexp.test(origin))) cb(null, true);
         else cb(new Error('Not allowed'), false);   
